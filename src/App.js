728d5f369a02e06c6edf8f1cfb867992bcc4f094
sheoplee <sheoplee@gmail.com>
2023-02-06 16:00:31 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 
 class App extends React.Component {
@@ -8,11 +8,51 @@ class App extends React.Component {
 
   }
 
+  customer = [
+    {
+      id: 1,
+      image: 'https://placeimg.com/32/32/1',
+      name: "홍길동",
+      birthday: "970219",
+      gender: "남자",
+      job: "대학생"
+    },
+    {
+      id: 2,
+      image: 'https://placeimg.com/64/64/2',
+      name: "김아무개",
+      birthday: "881219",
+      gender: "여자",
+      job: "회사원"
+    },
+    {
+      id: 3,
+      image: 'https://placeimg.com/64/64/3',
+      name: "이순신",
+      birthday: "781219",
+      gender: "남자",
+      job: "디자이너"
+    }
+  ]
+
   render () {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!!!</h2>
+      <div>
+        {
+          this.customer.map(c => {
+            return (
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+              />
+            )
+          })
+        }
       </div>
     );
   }
